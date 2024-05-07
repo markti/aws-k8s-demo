b6e3917b101e0e3efb4e6c28a506a0d5324cf5a9
markti <markti@outlook.com>
2024-05-07 19:29:09 -0400

    frontend backend

--- src/terraform/infra/container-registry.tf
+++ src/terraform/infra/container-registry.tf
@@ -1,5 +1,16 @@
-resource "aws_ecr_repository" "main" {
-  name                 = "ecr-${var.application_name}-${var.environment_name}"
+resource "aws_ecr_repository" "frontend" {
+  name                 = "ecr-${var.application_name}-${var.environment_name}-frontend"
+  image_tag_mutability = "MUTABLE"
+
+  tags = {
+    application = var.application_name
+    environment = var.environment_name
+  }
+
+}
+
+resource "aws_ecr_repository" "backend" {
+  name                 = "ecr-${var.application_name}-${var.environment_name}-backend"
   image_tag_mutability = "MUTABLE"
 
   tags = {
