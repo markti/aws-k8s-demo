93db69cb73dbdc384dffa8ac9714188b8c845490
markti <markti@outlook.com>
2024-05-07 20:57:33 -0400

    frontend_repository_url

--- src/terraform/infra/outputs.tf
+++ src/terraform/infra/outputs.tf
@@ -1,3 +1,6 @@
 output "frontend_repository_url" {
-  value = "foo"
+  value = aws_ecr_repository.main["frontend"].repository_url
+}
+output "backend_repository_url" {
+  value = aws_ecr_repository.main["backend"].repository_url
 }
