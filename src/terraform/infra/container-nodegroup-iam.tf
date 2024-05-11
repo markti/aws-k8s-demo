fff15868f0a754cb0d83cb77a99b84f849e4cd7e
markti <markti@outlook.com>
2024-05-11 14:13:27 -0400

    remove node group add sg

--- src/terraform/infra/container-nodegroup-iam.tf
+++ src/terraform/infra/container-nodegroup-iam.tf
@@ -19,17 +19,26 @@ resource "aws_iam_role" "container_node_group" {
   }
 }
 
+# EKS Worker Node
 resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.container_node_group.name
 }
 
+# CNI
 resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.container_node_group.name
 }
 
+# ECR
 resource "aws_iam_role_policy_attachment" "eks_ecr_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.container_node_group.name
+}
+
+# CloudWatch
+resource "aws_iam_role_policy_attachment" "cloudwatch" {
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
   role       = aws_iam_role.container_node_group.name
 }
---
+++ src/terraform/infra/network-security.tf
@@ -0,0 +1,48 @@
+
+resource "aws_security_group" "cluster_sg" {
+  name   = ""
+  vpc_id = aws_vpc.main.id
+
+  egress {
+    from_port = 0
+    to_port   = 0
+
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_security_group_rule" "cluster_ingress_https" {
+
+  security_group_id = aws_security_group.cluster_sg.id
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+}
+
+resource "aws_security_group_rule" "nodeport_cluster" {
+
+  security_group_id = aws_security_group.cluster_sg.id
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 30000
+  to_port           = 32768
+  description       = "nodeport"
+  protocol          = "tcp"
+
+}
+
+resource "aws_security_group_rule" "nodeport_cluster_udp" {
+
+  security_group_id = aws_security_group.cluster_sg.id
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 30000
+  to_port           = 32768
+  description       = "nodeport"
+  protocol          = "udp"
+
+}
