87e0a5b08bfe8235d7ec5f8c72eda4c15d5787ca
markti <markti@outlook.com>
2024-05-11 18:46:08 -0400

    fix nat

--- src/terraform/infra/network-backend.tf
+++ src/terraform/infra/network-backend.tf
@@ -41,8 +41,6 @@ resource "aws_nat_gateway" "nat" {
   for_each = local.private_subnets
 
   allocation_id = aws_eip.nat[each.key].id
-  subnet_id     = aws_subnet.backend[each.key].id
-
-  depends_on = [aws_internet_gateway.main]
+  subnet_id     = aws_subnet.frontend[each.key].id
 
 }
