b35a7a4a265d72462397376d225163edccea77ef
markti <markti@outlook.com>
2024-05-11 20:33:48 -0400

    fix outputs

--- src/terraform/infra/outputs.tf
+++ src/terraform/infra/outputs.tf
@@ -11,8 +11,8 @@ output "backend_repository_url" {
   value = aws_ecr_repository.main["backend"].repository_url
 }
 output "kubernetes_cluster_name" {
-  value = "aws_eks_cluster.main.name"
+  value = aws_eks_cluster.main.name
 }
 output "primary_region" {
-  value = "var.primary_region"
+  value = var.primary_region
 }
