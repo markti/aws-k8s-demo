2a826a9b3cdf0c596a013be49b2086346a3075e3
markti <markti@outlook.com>
2024-05-13 19:30:26 -0400

    admin group role

--- src/terraform/infra/admin-group-console-access.tf
+++ src/terraform/infra/admin-group-console-access.tf
@@ -1,3 +1,23 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role" "console_access" {
+
+  name = "${var.application_name}-${var.environment_name}-console-access"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ],
+  })
+}
+
 resource "aws_iam_policy" "console_access" {
   name        = "${var.application_name}-${var.environment_name}-console-access"
   description = "Allow users to assume the role to access EKS from the console"
