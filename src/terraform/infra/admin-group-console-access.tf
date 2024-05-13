dfdd5b3b155b10097d399b9b582629563457b76b
markti <markti@outlook.com>
2024-05-13 19:13:49 -0400

    console_access

--- src/terraform/infra/admin-group-console-access.tf
+++ src/terraform/infra/admin-group-console-access.tf
@@ -1,20 +1,16 @@
 resource "aws_iam_policy" "console_access" {
-  name        = "AssumeExampleRole"
-  description = "Allows assuming the specified EKS role"
+  name        = "${var.application_name}-${var.environment_name}-console-access"
+  description = "Allow users to assume the role to access EKS from the console"
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect   = "Allow",
-      Action   = "sts:AssumeRole",
-      Resource = "${aws_iam_role.console_access.arn}"
-    }]
-  })
+  policy = aws_iam_policy_document.console_access.json
 }
 
-resource "aws_iam_role" "console_access" {
-  name               = "${var.application_name}-${var.environment_name}-console-access"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+data "aws_iam_policy_document" "console_access" {
+  statement {
+    effect    = "Allow"
+    actions   = ["sts:AssumeRole"]
+    resources = aws_iam_role.console_access.arn
+  }
 }
 
 resource "aws_iam_group_policy_attachment" "console_access" {
