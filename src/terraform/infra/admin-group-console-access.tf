d3b086045736582370851160ab5153ebc8d59388
markti <markti@outlook.com>
2024-05-15 21:16:12 -0400

    console data access

--- src/terraform/infra/admin-group-console-access.tf
+++ src/terraform/infra/admin-group-console-access.tf
@@ -1,4 +1,40 @@
 data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "console_access" {
+  version = "2012-10-17"
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "eks:ListFargateProfiles",
+      "eks:DescribeNodegroup",
+      "eks:ListNodegroups",
+      "eks:ListUpdates",
+      "eks:AccessKubernetesApi",
+      "eks:ListAddons",
+      "eks:DescribeCluster",
+      "eks:DescribeAddonVersions",
+      "eks:ListClusters",
+      "eks:ListIdentityProviderConfigs",
+      "iam:ListRoles"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect    = "Allow"
+    actions   = ["ssm:GetParameter"]
+    resources = ["arn:aws:ssm:*:${data.aws_caller_identity.current.account_id}:parameter/*"]
+  }
+}
+
+resource "aws_iam_policy" "console_access" {
+
+  name        = "${var.application_name}-${var.environment_name}-console-access"
+  description = "Allow users to assume the role to access EKS from the console"
+  policy      = data.aws_iam_policy_document.console_access.json
+}
+
 /*
 resource "aws_iam_role" "console_access" {
 
@@ -18,12 +54,6 @@ resource "aws_iam_role" "console_access" {
   })
 }
 
-resource "aws_iam_policy" "console_access" {
-  name        = "${var.application_name}-${var.environment_name}-console-access"
-  description = "Allow users to assume the role to access EKS from the console"
-
-  policy = data.aws_iam_policy_document.console_access.json
-}
 
 data "aws_iam_policy_document" "console_access" {
   statement {
