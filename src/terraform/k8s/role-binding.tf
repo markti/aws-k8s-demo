f3e3a2093b9ffb424ac83fbeb6e378b603d11065
markti <markti@outlook.com>
2024-05-15 22:30:04 -0400

    remove

--- src/terraform/k8s/role-binding.tf
+++ src/terraform/k8s/role-binding.tf
@@ -1,3 +1,4 @@
+/*
 resource "kubernetes_cluster_role_v1" "eks_console_dashboard_full_access_clusterrole" {
   metadata {
     name = "eks-console-dashboard-full-access-clusterrole"
@@ -102,3 +103,4 @@ resource "kubernetes_cluster_role_binding_v1" "eks_console_dashboard_full_access
     api_group = "rbac.authorization.k8s.io"
   }
 }
+*/
