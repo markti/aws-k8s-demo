5ecedbae9a68482b300f99bf8503e8d10c17e5d1
markti <markti@outlook.com>
2024-05-15 21:31:36 -0400

    console access role

--- src/terraform/infra/admin-group-console-access.tf
+++ src/terraform/infra/admin-group-console-access.tf
@@ -36,36 +36,20 @@ resource "aws_iam_policy" "console_access" {
 
 }
 
-/*
-resource "aws_iam_role" "console_access" {
+data "aws_iam_policy_document" "console_access_assume_role_policy" {
+  version = "2012-10-17"
 
-  name = "${var.application_name}-${var.environment_name}-console-access"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ],
-  })
-}
-
-
-data "aws_iam_policy_document" "console_access" {
   statement {
-    effect    = "Allow"
-    actions   = ["sts:AssumeRole"]
-    resources = [aws_iam_role.console_access.arn]
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"] # Allow all users
+    }
+    actions = ["sts:AssumeRole"]
   }
 }
 
-resource "aws_iam_group_policy_attachment" "console_access" {
-  group      = aws_iam_group.admin.name
-  policy_arn = aws_iam_policy.console_access.arn
+resource "aws_iam_role" "console_access" {
+  name               = "${var.application_name}-${var.environment_name}-console-access"
+  assume_role_policy = data.aws_iam_policy_document.console_access_assume_role_policy.json
 }
-*/
