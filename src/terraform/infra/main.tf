604126da79abd281848af0ecaa13f73fcaa01703
markti <markti@outlook.com>
2024-05-27 17:20:50 -0400

    fix broken link

--- src/terraform/infra/main.tf
+++ src/terraform/infra/main.tf
@@ -28,18 +28,4 @@ data "aws_caller_identity" "current" {}
 
 locals {
   partition = data.aws_partition.current.partition
-
-  cluster_creator = {
-    principal_arn = data.aws_iam_session_context.current.issuer_arn
-    type          = "STANDARD"
-
-    policy_associations = {
-      admin = {
-        policy_arn = "arn:${local.partition}:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-        access_scope = {
-          type = "cluster"
-        }
-      }
-    }
-  }
 }
