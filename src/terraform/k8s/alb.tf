a2d67b86ac27a2023eac9f10de78b2d311eabaff
markti <markti@outlook.com>
2024-05-27 21:44:39 -0400

    add helm for alb

---
+++ src/terraform/k8s/alb.tf
@@ -0,0 +1,32 @@
+resource "kubernetes_service_account" "alb_controller" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+  }
+
+  automount_service_account_token = true
+}
+
+resource "helm_release" "alb_controller" {
+
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version    = "1.7.2"
+
+  set {
+    name  = "clusterName"
+    value = var.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+}
--- src/terraform/k8s/versions.tf
+++ src/terraform/k8s/versions.tf
@@ -32,3 +32,9 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.main.token
 }
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
