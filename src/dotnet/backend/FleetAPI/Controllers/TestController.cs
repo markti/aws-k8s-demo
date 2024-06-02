a9ae07697565fb02f7fb0fcf7843bfb7ee7423ec
markti <markti@outlook.com>
2024-06-01 22:03:17 -0400

    add test controller

---
+++ src/dotnet/backend/FleetAPI/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FleetAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetConnectionString")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            string dbConnStr = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+            return dbConnStr;
+        }
+    }
+}
--- src/dotnet/backend/FleetAPI/Controllers/WeatherForecastController.cs
+++ src/dotnet/backend/FleetAPI/Controllers/WeatherForecastController.cs
@@ -21,6 +21,8 @@ namespace FleetAPI.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            string dbConnStr = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
