bf9dcb372b21ae15d35bb3ec6d09da2194d879a6
markti <markti@outlook.com>
2024-06-01 20:56:26 -0400

    add spc

--- src/terraform/k8s/aws-secrets-manager.tf
+++ src/terraform/k8s/aws-secrets-manager.tf
@@ -16,3 +16,34 @@ resource "helm_release" "aws_secrets_provider" {
   namespace  = "kube-system"
 
 }
+
+locals {
+  secrets = {
+    "${var.application_name}-${var.environment_name}-connection-string" = "DB_CONNECTION_STRING"
+  }
+}
+
+resource "kubernetes_manifest" "secret_provider_class" {
+
+  manifest = {
+    apiVersion = "secrets-store.csi.x-k8s.io/v1"
+    kind       = "SecretProviderClass"
+    metadata = {
+      name      = "${var.application_name}-${var.environment_name}-secret-provider-class"
+      namespace = var.k8s_namespace
+    }
+    spec = {
+      provider = "aws"
+      parameters = {
+        objects = jsonencode([
+          for key, value in local.secrets : {
+            objectName         = key
+            objectType         = "secretsmanager"
+            objectVersionLabel = "AWSCURRENT"
+          }
+        ])
+      }
+    }
+  }
+
+}
