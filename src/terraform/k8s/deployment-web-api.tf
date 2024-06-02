13846e45b24e8660dc36e6d02176a0f465380cd5
markti <markti@outlook.com>
2024-06-01 21:44:59 -0400

    add csi and secret from env

--- src/terraform/k8s/deployment-web-api.tf
+++ src/terraform/k8s/deployment-web-api.tf
@@ -28,6 +28,17 @@ resource "kubernetes_deployment" "web_api" {
       spec {
         service_account_name = kubernetes_service_account.workload_identity.metadata[0].name
 
+        volume {
+          name = "secrets-store-inline"
+          csi {
+            driver    = "secrets-store.csi.k8s.io"
+            read_only = true
+            volume_attributes = {
+              "secretProviderClass" = kubernetes_manifest.secret_provider_class.metadata[0].name
+            }
+          }
+        }
+
         container {
           image = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.primary_region}.amazonaws.com/${var.web_api_image.name}:${var.web_api_image.version}"
           name  = local.web_api_name
@@ -37,6 +48,15 @@ resource "kubernetes_deployment" "web_api" {
           env_from {
             config_map_ref {
               name = kubernetes_config_map.web_api.metadata.0.name
+            }
+          }
+          env {
+            name = "DB_CONNECTION_STRING"
+            value_from {
+              secret_key_ref {
+                name = kubernetes_manifest.secret_provider_class.metadata[0].name
+                key  = "DB_CONNECTION_STRING"
+              }
             }
           }
         }
