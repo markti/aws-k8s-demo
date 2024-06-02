2e76f31b7496f0b865ad55e583754d6191f06b82
markti <markti@outlook.com>
2024-06-01 20:27:24 -0400

    aws provider

--- src/terraform/k8s/aws-secrets-manager.tf
+++ src/terraform/k8s/aws-secrets-manager.tf
@@ -1,15 +1,18 @@
 resource "helm_release" "csi_secrets_store" {
+
   name       = "csi-secrets-store"
   repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
   chart      = "secrets-store-csi-driver"
   namespace  = "kube-system"
+
 }
 
 
 resource "helm_release" "aws_secrets_provider" {
+
   name       = "secrets-provider-aws"
   repository = "https://aws.github.io/secrets-store-csi-driver-provider-aws"
-  chart      = "secrets-provider-aws"
+  chart      = "secrets-store-csi-driver-provider-aws"
   namespace  = "kube-system"
 
 }
